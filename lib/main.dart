347fa3d08ada111a20bcdfef8b5c1a31c83738c0
Farhan Dinur <farhan98@mhs.unsyiah.ac.id>
2020-12-13 21:18:26 +0700

    Welcomescreen_Sprint 1
    menambahkan welcomescreen.dart, dan mengubah beberapa baris di main.dart

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+//import 'package:login/guru/loginscreen.dart';
+import 'package:login/welcomescreen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarColor(Colors.green);
+    return MaterialApp(
+      title: "Parenting Control",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.green),
+      home: Welcomescreen(),
+    );
+  }
+}
